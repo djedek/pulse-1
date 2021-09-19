a366a70bc1d069969665194e68e4b291da494bc7
DjEdek <krivev10@gmail.com>
2021-09-19 15:59:40 +0300

    section-4(slick-slider) + mobil

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 300,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
