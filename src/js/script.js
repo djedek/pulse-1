944769417d94bcdddc7f1a9005337eb1f6a1e596
DjEdek <krivev10@gmail.com>
2021-09-22 14:50:35 +0300

    modal windows(mailer)

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,8 @@
 
 (function($) {
     $(function() {
+
+    // Tabs
       
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
@@ -60,12 +62,65 @@
                 $('.overlay, #order').fadeIn('slow');
             })
         });
+
+        // Validate
+
+        function valideForms(form) {
+            $(form).validate({
+                rules: {
+                    name: "required",
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: "Пожалуйста, введите своё имя",
+                    phone: "Пожалуйста, введите свой номер телефона",
+                    email: {
+                        required: "Пожалуйста, введите свой E-mail",
+                        email: "Неправильно введён E-mail"
+                    }
+                }
+    
+            });
+        };
+
+        valideForms('#consultation-form');
+        valideForms('#consultation form');
+        valideForms('#order form');
+
+
+        // valide phone mask
+
+        $('input[name=phone]').mask("+3 8(999) 999-99-99");
+
+
+        // Mailer
+
+        $('form').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+    
+                $('form').trigger('reset');
+            });
+            return false;
+        });
+
       
     });
 })(jQuery);
 
 
-
+//  Tiny-slider
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
