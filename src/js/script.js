f68736d58766ecd83e2e04e4fc91a17a436fee88
DjEdek <krivev10@gmail.com>
2021-09-23 15:55:53 +0300

    wow    animate

--- src/js/script.js
+++ src/js/script.js
@@ -115,6 +115,26 @@
             return false;
         });
 
+
+        // Smuth scroll and pageup
+
+        $(window).scroll(function() {
+            if($(this).scrollTop() > 1600) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+
+        $("a[href^='#']").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
+
+
+        new WOW().init();
+
       
     });
 })(jQuery);
