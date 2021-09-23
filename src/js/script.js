f7d54f79d8d4567dc438f35e8040a5af019cc472
DjEdek <krivev10@gmail.com>
2021-09-23 16:48:53 +0300

    validator

--- src/js/script.js
+++ src/js/script.js
@@ -126,7 +126,12 @@
             }
         });
 
-        $("a[href^='#']").click(function(){
+        $("a[href^='#up']").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
+        $("a[href^='#catalog']").click(function(){
             const _href = $(this).attr("href");
             $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
             return false;
